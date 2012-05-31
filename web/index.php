da223c12ea26e7d8b24b796e289f034cb43a1433
choffmeister <thekwasti@googlemail.com>
2012-05-31 15:50:54 +0200

    added view for groups

--- web/index.php
+++ web/index.php
@@ -22,26 +22,58 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.common.class_path' => __DIR__.'/../vendor/doctrine-common/lib',
 ));
 $app['debug'] = true;
+ini_set('display_errors', 1);
 
-$app->get('/', function() use($app, $jade) {
-    return $app['twig']->render('index.twig', array(
-        'name' => $name,
-    ));
+$app->get('/', function() use($app) {
+    return $app->redirect('/groups');
 });
 
-$app->get('/groups', function(Request $request) use($app, $jade) {
+$app->get('/groups', function() use($app) {
     $groups = $app['db']->fetchAll('SELECT * FROM groups');
 
-    return new JsonResponse($groups);
-});
-
-$app->get('/eq/{equation}', function($equation) use($app, $jade) {
-    return $app['twig']->render('equation.twig', array(
-        'equation' => $equation,
+    return $app['twig']->render('groups.twig', array(
+        'groups' => $groups,
     ));
 });
 
-$app->error(function(Exception $exception, $code) use($app, $jade) {
+$app->get('/group/{id}', function($id) use($app) {
+    $group = $app['db']->fetchAssoc('SELECT * FROM groups WHERE id = ?', array($id));
+
+    if (!$group) {
+        return $app->abort(404, sprintf('Unknown group id %d', $id));
+    }
+
+    $k = 0;
+    $upperTriangle = json_decode($group['matrix']);
+    $matrix = array();
+
+    for ($i = 0; $i < $group['rank']; $i++) $matrix[] = array();
+
+    for ($i = 0; $i < $group['rank']; $i++) {
+        $matrix[$i][$i] = 1;
+
+        for ($j = $i + 1; $j < $group['rank']; $j++) {
+            $matrix[$i][$j] = $upperTriangle[$k];
+            $matrix[$j][$i] = $upperTriangle[$k];
+            $k++;
+        }
+    }
+
+    $group['matrix'] = $matrix;
+
+    $automorphisms = $app['db']->fetchAll('SELECT * FROM automorphisms WHERE group_id = ?', array($id));
+
+    for ($i = 0; $i < count($automorphisms); $i++) {
+        $automorphisms[$i]['transpositions'] = json_decode($automorphisms[$i]['transpositions']);
+    }
+
+    return $app['twig']->render('group.twig', array(
+        'group' => $group,
+        'automorphisms' => $automorphisms,
+    ));
+});
+
+$app->error(function(Exception $exception, $code) use($app) {
     $title = isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : Response::$statusTexts[500];
 
     return new Response($app['twig']->render('error.twig', array(
